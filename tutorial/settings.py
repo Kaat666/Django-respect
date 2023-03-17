d65ff7b9b523457cda95e4273305f464942f108c
Kaat666 <bolgovn92@gmail.com>
2023-03-17 18:24:12 +0300

    Gege

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'product',
     'order',
     'category',
+    'user',
 ]
 
 MIDDLEWARE = [
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -38,6 +38,7 @@ urlpatterns = [
     path("", include("category.urls")),
     path("", include("order.urls")),
     path("", include("product.urls")),
+    path("", include("user.urls")),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
