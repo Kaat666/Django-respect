4f5a356e6d908661ef54adbc3ccf2b5aeb895e0d
Kaat666 <62111611+Kaat666@users.noreply.github.com>
2023-03-24 18:31:42 +0300

    Update

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -139,7 +139,3 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
-
-SWAGGER_SETTINGS = {
-    'VALIDATOR_URL': 'http://localhost:8189',
-}
