bcd2700bf85378790a1c7d2d259b09bf4cb3b7cd
Kaat666 <62111611+Kaat666@users.noreply.github.com>
2023-03-31 12:31:59 +0300

    Delete order directory

--- order/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from order import views
-from rest_framework.urlpatterns import format_suffix_patterns
-
-urlpatterns = [
-    path("order/", views.OrderView.as_view()),
-    path("order/<int:products_id>/", views.OrderDetail.as_view()),
-]
-urlpatterns = format_suffix_patterns(urlpatterns)
