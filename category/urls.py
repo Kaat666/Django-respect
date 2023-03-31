7abe94fd24fdf08f452815d8e5d66ef3b9d208d3
Kaat666 <62111611+Kaat666@users.noreply.github.com>
2023-03-31 12:30:59 +0300

    Delete category directory

--- category/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from category import views
-from rest_framework.urlpatterns import format_suffix_patterns
-
-urlpatterns = [
-    path("category/<int:category_id>/", views.CategoriesProduct.as_view()),
-    path("category/", views.CategoryView.as_view()),
-]
-urlpatterns = format_suffix_patterns(urlpatterns)
