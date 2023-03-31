d03110024f8a1a92e2fecf406b3f0718895541c8
Kaat666 <62111611+Kaat666@users.noreply.github.com>
2023-03-31 12:30:50 +0300

    Delete cart directory

--- cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from cart import views
-from rest_framework.urlpatterns import format_suffix_patterns
-
-urlpatterns = [
-    path("cart/", views.CartView.as_view()),
-    path("cart/<int:pk>/", views.CartDetail.as_view()),
-]
-urlpatterns = format_suffix_patterns(urlpatterns)
